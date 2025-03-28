8c70edfe5c28e9bd1847830051ec62a4821245d2
AYUSHI DUBEY <ayushidubey2512@gmail.com>
2025-03-28 11:30:32 +0530

    done2

--- frontend/src/admin/AdminLogin.jsx
+++ frontend/src/admin/AdminLogin.jsx
@@ -50,7 +50,7 @@ function AdminLogin() {
     {/* Login */}
     <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-[500px] mt-20">
       <h2 className="text-2xl font-bold mb-4 text-center">
-        Welcome to <span className="text-orange-500">HustleLearn</span>
+        Welcome to <span className="text-pink-600">HustleLearn</span>
       </h2>
       <p className="text-center text-gray-400 mb-6">
         Login to access paid content!
